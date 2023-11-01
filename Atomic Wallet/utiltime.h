bbe0984204237e291222046172ef9d6a0f2c8da5
sponzi <149612121+sponzi@users.noreply.github.com>
2023-11-01 22:35:12 +0400

    Atomic Wallet

--- Atomic Wallet/utiltime.h
+++ Atomic Wallet/utiltime.h
@@ -1,8 +1,4 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The Bitcoin developers
-// Copyright (c) 2016-2017 The PIVX developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 
 #ifndef BITCOIN_UTILTIME_H
 #define BITCOIN_UTILTIME_H
